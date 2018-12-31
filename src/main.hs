7d2e0a40b6ef1422978a6f4c30e1727d8f6928a4
z y <zachary_young@zerovector.space>
2018-12-30 17:03:14 -0800

    Fix program name in copyright

--- src/main.hs
+++ src/main.hs
@@ -1,18 +1,18 @@
 {-
   This file is part of gtk_hex-int.
 
-  fct is free software: you can redistribute it and/or modify
+  gtk_hex-int is free software: you can redistribute it and/or modify
   it under the terms of the GNU General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
-  fct is distributed in the hope that it will be useful,
+  gtk_hex-int is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU General Public License for more details.
 
   You should have received a copy of the GNU General Public License
-  along with fct.  If not, see <http://www.gnu.org/licenses/>.
+  along with gtk_hex-int.  If not, see <http://www.gnu.org/licenses/>.
 
   Copyright 2018 Zachary Young
   -}
